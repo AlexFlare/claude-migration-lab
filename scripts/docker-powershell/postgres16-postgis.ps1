9ced99b51c4960b2055c46df7a5449544541e3de
oleksandr-lavreniuk <alexander.lavrenyuk@gmail.com>
2026-05-13 19:49:48 +0300

    Update postgres16-postgis by ltree, citext, pgcrypto extensions

--- scripts/docker-powershell/postgres16-postgis.ps1
+++ scripts/docker-powershell/postgres16-postgis.ps1
@@ -1,13 +1,13 @@
 docker run --name postgres16-postgis `
-  --network dev-net `
   -e POSTGRES_USER=postgres `
   -e POSTGRES_PASSWORD=postgrespass `
   -e POSTGRES_DB=postgres `
   -p 5432:5432 `
-  -v postgres16_postgis_data:/var/lib/postgresql/data `
+  -v postgres_postgis_data:/var/lib/postgresql/data `
   -d postgis/postgis:16-3.5
 
-Start-Sleep -Seconds 8
+Start-Sleep -Seconds 10
 
-docker exec postgres16-postgis psql -U postgres -d postgres `
-  -c "CREATE EXTENSION IF NOT EXISTS postgis; SELECT PostGIS_Version();"
+docker exec postgres16-postgis psql -U postgres -d postgres -c "CREATE EXTENSION IF NOT EXISTS postgis; CREATE EXTENSION IF NOT EXISTS ltree; CREATE EXTENSION IF NOT EXISTS citext; CREATE EXTENSION IF NOT EXISTS pgcrypto;"
+
+docker exec postgres16-postgis psql -U postgres -d postgres -c "SELECT extname, extversion FROM pg_extension ORDER BY extname;"
